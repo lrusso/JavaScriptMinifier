c27a645349ebb2e91f72cef6455b5349df2ee1cd
Leonardo Russo <info@lrusso.com>
2020-12-26 23:49:26 -0300

    Add files via upload

---
+++ worker.js
@@ -0,0 +1,33 @@
+const filesToCache = [
+	"JavaScriptMinifier.htm",
+	"JavaScriptMinifier.json",
+	"JavaScriptMinifier.png",
+	"JavaScriptMinifierFavIcon_16x16.png",
+	"JavaScriptMinifierFavIcon_192x192.png",
+	"JavaScriptMinifierFavIcon_512x512.png",
+	"JavaScriptMinifierShare.png"
+];
+
+const staticCacheName = "JavaScriptMinifier-v1";
+
+self.addEventListener("install", event => {
+	event.waitUntil(
+		caches.open(staticCacheName)
+		.then(cache => {
+			return cache.addAll(filesToCache);
+		})
+	);
+});
+
+self.addEventListener("fetch", event => {
+	event.respondWith(
+		caches.match(event.request)
+		.then(response => {
+			if (response) {
+				return response;
+			}
+			return fetch(event.request)
+		}).catch(error => {
+		})
+	);
+});
